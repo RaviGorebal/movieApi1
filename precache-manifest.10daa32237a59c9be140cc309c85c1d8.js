185ae2c6276dc267b8770ae9b6663a9752a3f36a
Ravi Kumar G <ravikumarg@maveric-systems.com>
2020-07-07 12:08:05 +0530

    Updates

---
+++ precache-manifest.10daa32237a59c9be140cc309c85c1d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8922944539f13199daeb6cdfa7bcd81",
+    "url": "/Movie-Database/index.html"
+  },
+  {
+    "revision": "e21ed2204ea090586e81",
+    "url": "/Movie-Database/static/css/main.97323b1d.chunk.css"
+  },
+  {
+    "revision": "db50709b9fb81f63b268",
+    "url": "/Movie-Database/static/js/2.fa084981.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Movie-Database/static/js/2.fa084981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e21ed2204ea090586e81",
+    "url": "/Movie-Database/static/js/main.ba595789.chunk.js"
+  },
+  {
+    "revision": "1d4a7c3a66337f4267f8",
+    "url": "/Movie-Database/static/js/runtime-main.4d21f6ab.js"
+  },
+  {
+    "revision": "f362aeab7bccfbb6f6cd8fc7a894213a",
+    "url": "/Movie-Database/static/media/movie.f362aeab.png"
+  }
+]);
